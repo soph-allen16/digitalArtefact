0fb152fa94a889b2edb5186573a236eb42af80ba
Sophie Barlow <23758045@stu.mmu.ac.uk>
2024-02-13 14:06:19 +0000

    completed challenge exercise

---
+++ src/challenges/challenge1/Controller.java
@@ -0,0 +1,7 @@
+package challenges.challenge1;
+
+public class Controller {
+    public static void main(String[] args){
+        Code code = new Code();
+    }
+}
--- src/challenges/challengeExercise1/CodedMessage.java
+++
@@ -1,2 +0,0 @@
-package challenges.challengeExercise1;public class CodedMessage {
-}
