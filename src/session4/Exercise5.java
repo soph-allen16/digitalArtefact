e5f1e02371ee7c8bbba25a7a6142bc72b3afc6a9
Sophie Barlow <23758045@stu.mmu.ac.uk>
2024-02-20 13:12:50 +0000

    finished exercises

--- src/session4/Exercise5.java
+++ src/session4/Exercise5.java
@@ -1,7 +1,27 @@
 package session4;
 
+import java.util.Scanner;
+
 public class Exercise5 {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int randomNumber = (int) (Math.random() * 10) +1;
 
+        System.out.println("please enter a guess between 1 and 10:");
+        int guessNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        while( guessNumber != randomNumber){
+            if( (guessNumber>10)||(guessNumber<1)){
+                System.out.println("Please enter a number between 1 and 10");
+            }else{
+                System.out.println("Wrong! Please try again");
+            }
+            guessNumber = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        System.out.println("Well done!");
+        scanner.close();
     }
 }
